10ba0aa083710eb178f37e8132ef483667389ded
jeoffrey0522 <jeoffrey0522@naver.com>
2016-06-11 22:56:10 +0900

    plotly

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-
+import startup
 
 def project(x,pn,pd):
 	return x-pn*(np.dot(x,pn)-pd)
 
 def clip_sphere(x,r=0.5):
-	return np.linalg.norm(x)<=r
+	return np.linalg.norm(x, axis=0)<=r
 
 def clip_cube(x,r=0.5):
-    return np.all(np.abs(x[0])<=r, \
-                  np.abs(x[1])<=r, \
-                  np.abs(x[2])<=r)
+    return np.all((np.abs(x[0])<=r, 
+                  np.abs(x[1])<=r, 
+                  np.abs(x[2])<=r),
+                  axis=0)
 
 def clip_column_sphere(x, pn,r=0.5):
-	return np.linalg.norm(np.cross(x-(pn+c),x-c))<=r
+	return np.linalg.norm(np.cross(x.T-pn,x.T), axis=1)<=r
 		
 def clip_column_square_xy(x, pn, r=0.5, h=0):
     t=(h-x[2])/pn[2]
@@ -30,13 +31,14 @@ def clip_column_square_zx (x, pn, r=0.5, h=0):
                           np.abs(x[2]+pn[2]*t)<=r)
 
 def clip_column_cube(x, pn, r=0.5):
-    return np.logical_or.reduce(
-        (clip_column_square_xy(x,pn,r,0.5),
+    return np.any((
+        clip_column_square_xy(x,pn,r,0.5),
         clip_column_square_xy(x,pn,r,-0.5),
         clip_column_square_yz(x,pn,r,0.5),
         clip_column_square_yz(x,pn,r,-0.5),
         clip_column_square_zx(x,pn,r,0.5),
-        clip_column_square_zx(x,pn,r,-0.5)))
+        clip_column_square_zx(x,pn,r,-0.5)),
+        axis=0)
 
 print("simulating...")
 planes_normal=[]
@@ -55,6 +57,8 @@ n=len(points)
 match=[0]*n_planes
 i=1
 
+def clip(x):
+    return clip_cube(x, 0.5)
 def clip_column(x,pn):
     return clip_column_cube(x, pn, 0.5)
 
@@ -70,50 +74,30 @@ for pn in planes_normal:
 	i+=1
 
 
-points=points[:1000]
+# plotting
 
-trace=go.Scatter3d(x=points[0], 
-                   y=points[1], 
-                   z=points[2],
-                   mode='markers',
-                   marker=dict(
-                       size=1.5,
-                       opacity=0.8
-                     ))
+points=points[:500]
+x,y,z=points[0],points[1],points[2]
+
+trace=go.Scatter3d(
+    x=x, y=y, z=z,
+    mode='markers',
+    marker=dict(
+        color=np.where(clip(points),0,1),
+        colorscale=[
+            [0, 'rgb(226, 157, 38)'],
+            [1, 'rgb(63, 127, 191)']
+            ],
+        size=1,opacity=0.8)
+    )
 data=[trace]
-layout=go.Layout(scene=dict(
-    xaxis=dict(range=[-0.6,0.6]),
-    yaxis=dict(range=[-0.6,0.6]),
-    zaxis=dict(range=[-0.6,0.6]))
+layout=go.Layout(
+    scene=dict(
+        xaxis=dict(range=[-0.6,0.6]),
+        yaxis=dict(range=[-0.6,0.6]),
+        zaxis=dict(range=[-0.6,0.6]))
     )
 fig=dict(data=data,layout=layout)
-py.iplot(fig,filename='scatter-test')
-
-
-"""
-import matplotlib.pyplot as plt
-import matplotlib.tri as mtri
-from mpl_toolkits.mplot3d import Axes3D
-
-fig = plt.figure()
-ax=plt.subplot2grid((2,2),(1,0),colspan=2)
-ax.axis([1,n_planes,0,1.05])
-ax.plot([1,1],[n_planes,1], label='expected')
-
-
-ax.plot(range(1,n_planes+1),match)
-
-ax = plt.subplot2grid((2,2),(0,0),projection='3d')
-ax.axis([-0.5,0.5]*2)
-ax.set_zlim(-0.5,0.5)
-x,y,z=zip(*points[:100].T)
-ax.scatter(x,y,z,s=3,c='b')
-
-ax=plt.subplot2grid((2,2),(0,1),projection='3d')
-x,y,z=zip(*planes_normal[:20])
-ax.quiver(0,0,0,x,y,z,pivot='tail')
-ax.set_xlim([-1,1])
-ax.set_ylim([-1,1])
-ax.set_zlim([-1,1])
-		  
-plt.show()"""
+#py.plot(fig,filename='scatter-test')
+print("plotting...")
+plotly.offline.plot(fig,filename='scatter-test.html')
