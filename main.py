5923b470f4efbe2a35e8b54faba527f8bf569dcf
jeoffrey0522 <jeoffrey0522@naver.com>
2016-06-13 21:45:29 +0900

    shapes operator overloading

--- main.py
+++ main.py
@@ -6,78 +6,137 @@ import math
 import itertools
 import numpy as np
 
+def simulate_volume(iterations, shape):
+    points=np.random.uniform(-1,1,size=(3,iterations))
+    samples=shape.clip(points)
+    n_samples=sum(samples)
+    return 8.0*n_samples/iterations
 
-def project(x,pn,pd):
-	return x-pn*(np.dot(x,pn)-pd)
 
+def simulate_inv_projection(iterations, planes_normal, shape):
+    points=np.random.uniform(-1,1,size=(3,iterations))
+    
+    v_original=simulate_volume(iterations, shape)
+
+    i=0
+    volume=[None]*len(planes_normal)
+    for pn in planes_normal:
+        points=points.T[shape.clip_cylinder(points, pn)].T
+        n_sample=points[0].size
+        volume[i]=8.0*n_sample/iterations
+        if(i%(len(planes_normal)/3)==0):
+            print("progress: {0:%}".format(i/len(planes_normal)))
+        i+=1
+    v=volume[-1]
+    print('\n'.join(('original volume:{0:f}','recovered:{1:f}','ratio:{2:f}')).format(v_original,v,v_original/v))
+    return points, v
+
+iterations=100000
+n_planes=50
+
+
+shapes={
+    'cube':sp.Cube(radius=0.5),
+    'rect':sp.Rect([-0.5,-0.5,0],[1,0,0],[0,1,0]),
+    'xy, yz, zx rect':
+        sp.Rect([-0.5,-0.5,0],[1,0,0],[0,1,0])|
+        sp.Rect([0,-0.5,-0.5],[0,1,0],[0,0,1])|
+        sp.Rect([-0.5,0,-0.5],[1,0,0],[0,0,1]),
+    'circle':sp.Circle([0,0,0],0.5,[1,0,0]),
+    'xy, yz circle':sp.Union((
+        sp.Circle([0,0,0],0.5,[1,0,0]),
+        sp.Circle([0,0,0],0.5,[0,1,0]))),
+    'xy, yz,zx circle':sp.Union((
+        sp.Circle([0,0,0],0.5,[1,0,0]),
+        sp.Circle([0,0,0],0.5,[0,1,0]),
+        sp.Circle([0,0,0],0.5,[0,0,1]))),
+    'sphere':sp.Sphere(radius=0.5),
+    '2 sphere intersection':sp.Intersection((
+        sp.Sphere([-0.25,0,0],0.5),
+        sp.Sphere([0.25,0,0],0.5))),
+    '2 sphere union':sp.Union((
+        sp.Sphere([-0.25,0,0],0.5),
+        sp.Sphere([0.25,0,0],0.5))),
+    '3 sphere union':sp.Union((
+        sp.Sphere([-0.25,0,0],0.5),
+        sp.Sphere([0.25,0,0],0.5),
+        sp.Sphere([0,0.4,0],0.5)))
+}
 
 print("simulating...")
-n_planes=1000
-iterations=1000000
 
-planes_normal=[]
-for _ in itertools.repeat(None, n_planes):
-	pn=np.random.uniform(-1,1, size=3)
-	pn=pn/np.linalg.norm(pn)
-	planes_normal.append(pn)
-#print(*planes_normal, sep='\n')
+phi=np.random.uniform(0,2*np.pi, size=n_planes)
+costheta=np.random.uniform(-1,1, size=n_planes)
+theta = np.arccos(costheta)
+planes_normal=np.vstack((
+    np.sin(theta)*np.cos(phi),
+    np.sin(theta)*np.sin(phi),
+    np.cos(theta))).T
 
-
-points=np.random.uniform(-1,1,size=(3,iterations))
-n=points[0].size
-
-#shape=sp.Cube(np.array([0.2,0.2,0.2]),0.4)
-circle_normal=np.array([1,0.5,0.2])
-circle_normal/=np.linalg.norm(circle_normal)
-
-shape=sp.Union((
-    sp.Circle(np.array([0,0,0]),0.5,np.array([1,0,0])),
-    sp.Circle(np.array([0,0,0]),0.5,np.array([0,1,0])),
-    sp.Circle(np.array([0,0,0]),0.5,np.array([0,0,1]))))
-
-match=[0]*len(planes_normal)
-
-i=1
-for pn in planes_normal:
-    
-	points=points.T[shape.clip_cylinder(points, pn)].T
-	n_sample=points[0].size
-
-	v=0
-	v_=8.0*n_sample/n
-	match[i-1]=1+(v-v_)/2**3
-	print('match rate: {0:%}, -log(1-x):{1:f}'.format(match[i-1],-math.log(1-match[i-1])))
-
-	i+=1
+i=0
+for shape_name, shape in shapes.items():
+    print('shape: '+shape_name)
+    points,_=simulate_inv_projection(iterations, planes_normal, shape)
 
 
-# plotting
-print("plotting...")
-import plotly
-import plotly.plotly as py
-import plotly.graph_objs as go
+    # plotting
+    print("plotting...")
+    import plotly
+    import plotly.plotly as py
+    import plotly.graph_objs as go
+    from IPython.display import Image
 
-points=points[:,:6000]
-x,y,z=points[0],points[1],points[2]
+    points=points[:,:1000]
+    x,y,z=points[0],points[1],points[2]
+    pnx,pny,pnz=planes_normal[:,0],planes_normal[:,1],planes_normal[:,2]
 
-trace=go.Scatter3d(
-    x=x, y=y, z=z,
-    mode='markers',
-    marker=dict(
-        color=np.where(shape.clip(points),0,1),
-        colorscale=[
-            [0, 'rgb(226, 157, 38)'],
-            [1, 'rgb(63, 127, 191)']
-            ],
-        size=1.5,opacity=0.4)
-    )
-data=[trace]
-layout=go.Layout(
     scene=dict(
         xaxis=dict(range=[-1,1]),
         yaxis=dict(range=[-1,1]),
-        zaxis=dict(range=[-1,1]))
-    )
-fig=dict(data=data,layout=layout)
-#py.plot(fig,filename='scatter-test')
-plotly.offline.plot(fig,filename='scatter_test.html')
+        zaxis=dict(range=[-1,1]),
+        camera=dict(
+            up=dict(x=0, y=0, z=1),
+            center=dict(x=0, y=0, z=0),
+            eye=dict(x=1, y=1.5, z=0.5)
+            ))
+
+    #fig=plotly.tools.make_subplots(2,3, specs=[[{'is_3d': True, 'rowspan':2, 'colspan':2},None, {'is_3d': True}],
+     #                                          [None,None,None]])
+    fig=plotly.tools.make_subplots(1,1, specs=[[{'is_3d': True}]])
+    marker=dict(
+            color=np.where(shape.clip(points),0,1),
+            colorscale=[
+                [0, 'rgb(226, 157, 38)'],
+                [1, 'rgb(63, 127, 191)']],
+            size=1.5,opacity=0.4)
+
+    fig.append_trace(go.Scatter3d(
+        x=x, y=y, z=z,
+        mode='markers',
+        marker=marker,
+        scene='scene1'),1,1)
+
+    '''fig.append_trace(go.Scatter3d(
+        x=pnx, y=pny, z=pnz,
+        mode='markers', marker=dict(size=5,opacity=0.9),
+        scene='scene1'),
+        1,3)'''
+
+    proj_normal=np.array([[1,0,0],[0,1,0],[0,0,1]])
+    for pn in proj_normal:
+        projected=sp.project(points, pn, pn*(-1)).T
+        fig.append_trace(go.Scatter3d(
+        x=projected[0], y=projected[1], z=projected[2],
+        mode='markers', marker=marker,
+        scene='scene1'),1,1)
+
+
+    fig['layout'].update(title=shape_name)
+    fig['layout']['scene1'].update(scene)
+    filename='test_'+shape_name.replace(' ','_')
+    #py.plot(fig,filename='scatter-test')
+    plotly.offline.plot(fig,filename='out\\'+'_'+filename+'.html',auto_open=False)
+    py.image.save_as(fig, filename='out\\'+filename+'.jpeg',width=1024,height=1024)
+    #Image('test.png')
+    
+    i+=1
--- shape.py
+++ shape.py
@@ -1,30 +1,36 @@
 import numpy as np
 
+def project(x,pn,c):
+        x=x.T-c
+        t=-np.dot(pn, x.T)
+        return x+np.outer(t,pn)+c
+
 class Base:
-    def __init__(self): pass
-    def clip(s, x): pass
-    def clip_cylinder(s, x, pn): pass
+    def __init__(self): raise NotImplementedError()
+    def clip(self, x): raise NotImplementedError()
+    def clip_cylinder(self, x, pn): raise NotImplementedError()
+
+    def __or__(self, other): return Union((self, other))
+    def __and__(self, other): return Intersection((self, other))
+    def __sub__(self,other): return Difference(self, other)
 
 class Empty(Base):
     def __init__(self): pass
-    def clip(s, x): 
-        return np.zeros_like(x[0], dtype=bool)
-    def clip_cylinder(s, x, pn): 
-        return np.zeros_like(x[0], dtype=bool)
+    def clip(self, x): return np.zeros_like(x[0], dtype=bool)
+    def clip_cylinder(self, x, pn): return np.zeros_like(x[0], dtype=bool)
     
 class Rect(Base):
-    def __init__(s, origin, axisA, axisB):
-        s.origin=origin
-        s.axisA=axisA
-        s.axisB=axisB
-    def clip(s, x):
-        Empty.clip(s, x)
-    def clip_cylinder(s, x, pn):
-        mat=np.vstack((s.axisA, s.axisB,-pn)).T
-        if np.linalg.det(mat)==0: return Empty.clip_cylinder(s,x,pn)
+    def __init__(self, origin, axisA, axisB):
+        self.origin=np.array(origin)
+        self.axisA=np.array(axisA)
+        self.axisB=np.array(axisB)
+    def clip(self, x): return Empty.clip(self, x)
+    def clip_cylinder(self, x, pn):
+        mat=np.vstack((self.axisA, self.axisB,-pn)).T
+        if np.linalg.det(mat)==0: return Empty.clip_cylinder(self,x,pn)
         invmat=np.linalg.inv(mat)
 
-        uvt=np.dot(invmat, (x.T-s.origin).T)
+        uvt=np.dot(invmat, (x.T-self.origin).T)
         u,v=uvt[0],uvt[1]
         return np.all((
             0<=u, u<=1,
@@ -32,21 +38,26 @@ class Rect(Base):
             axis=0)
 
 class Circle(Base):
-    def __init__(s, origin, radius, normal):
-        s.c=origin
-        s.r=radius
-        s.n=normal
-    def clip(s, x):
-        Empty.clip(s, x)
-    def clip_cylinder(s, x, pn):
-        x=x.T-s.c
-        t=-np.dot(s.n, x.T)/np.dot(s.n, pn.T)
-        return np.linalg.norm(x+np.outer(t,pn), axis=1)<=s.r
+    def __init__(self, origin, radius, normal):
+        self.c=np.array(origin)
+        self.r=radius
+        self.n=np.array(normal)
+    def clip(self, x):
+        return Empty.clip(self, x)
+    def clip_cylinder(self, x, pn):
+        x=x.T-self.c
+        div=np.dot(self.n, pn.T)
+        div=div if div!=0 else 0.001
+        t=-np.dot(self.n, x.T)/div
+        return np.logical_and(
+            np.isfinite(t),
+            np.linalg.norm(x+np.outer(t,pn), axis=1)<=self.r
+            )
 
 class Box(Base):
-    def __init__(self, origin=np.array([-0.5,-0.5,-0.5]), size=np.array([1,1,1])):
-        self.origin=origin
-        self.size=size
+    def __init__(self, origin=[-0.5,-0.5,-0.5], size=[1,1,1]):
+        self.origin=np.array(origin)
+        self.size=np.array(size)
         self.bound=np.vstack((origin, origin+size))
         xbasis=np.array([1,0,0])
         ybasis=np.array([0,1,0])
@@ -59,38 +70,50 @@ class Box(Base):
             Rect(origin,size*xbasis,size*ybasis),
             Rect(origin+size*zbasis,size*xbasis,size*ybasis)
             ]
-    def clip(s, x):
-        return np.all((s.bound[0]<=x.T, x.T<=s.bound[1]), axis=(0,2))
-    def clip_cylinder(s, x, pn):
-        return np.any([f.clip_cylinder(x,pn) for f in s.faces], axis=0)
+    def clip(self, x):
+        return np.all((self.bound[0]<=x.T, x.T<=self.bound[1]), axis=(0,2))
+    def clip_cylinder(self, x, pn):
+        return np.any([f.clip_cylinder(x,pn) for f in self.faces], axis=0)
 
 class Cube(Box):
-    def __init__(self, center=np.array([0,0,0]), radius=0.5):
-        Box.__init__(self, center-radius,radius*2)
+    def __init__(self, center=[0,0,0], radius=0.5):
+        Box.__init__(self, np.array(center)-radius,radius*2)
 
 class Sphere(Base):
-    def __init__(self, origin=np.array([0,0,0]), radius=0.5):
+    def __init__(self, origin=[0,0,0], radius=0.5):
+        self.c=np.array(origin)
         self.r=radius
-        self.c=origin
 
-    def clip(s, x):
-        return np.linalg.norm(x.T-s.c, axis=1)<=s.r
+    def clip(self, x):
+        return np.linalg.norm(x.T-self.c, axis=1)<=self.r
 
-    def clip_cylinder(s, x, pn):
-        return np.linalg.norm(np.cross(x.T-(pn+s.c),x.T-s.c), axis=1)<=s.r
+    def clip_cylinder(self, x, pn):
+        return np.linalg.norm(np.cross(x.T-(pn+self.c),x.T-self.c), axis=1)<=self.r
 
 class Union(Base):
     def __init__(self, shapes):
         self.shapes=list(shapes)
-    def clip(s, x):
-        return np.any([shape.clip(x) for shape in s.shapes], axis=0)
-    def clip_cylinder(s,x,pn):
-        return np.any([shape.clip_cylinder(x,pn) for shape in s.shapes], axis=0)
+    def clip(self, x):
+        return np.any([shape.clip(x) for shape in self.shapes], axis=0)
+    def clip_cylinder(self,x,pn):
+        return np.any([shape.clip_cylinder(x,pn) for shape in self.shapes], axis=0)
 
 class Intersection(Base):
     def __init__(self, shapes):
         self.shapes=list(shapes)
-    def clip(s, x):
-        return np.all([shape.clip(x) for shape in s.shapes], axis=0)
-    def clip_cylinder(s,x,pn):
-        return np.all([shape.clip_cylinder(x,pn) for shape in s.shapes], axis=0)
+    def clip(self, x):
+        return np.all([shape.clip(x) for shape in self.shapes], axis=0)
+    def clip_cylinder(self,x,pn):
+        return np.all([shape.clip_cylinder(x,pn) for shape in self.shapes], axis=0)
+
+class Difference(Base):
+    def __init__(self, shape1, shape2):
+        self.shape=Intersection((shape1,Inverse(shape2)))
+
+class Inverse(Base):
+    def __init__(self, shape):
+        self.shape=shape
+    def clip(self, x):
+        return np.logical_not(self.shape.clip(x), dtype=bool)
+    def clip_cylinder(self, x):
+        return np.logical_not(self.shape.clip_cylinder(x, pn), dtype=bool)
